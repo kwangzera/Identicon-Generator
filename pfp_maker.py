4b30db3cc8791dd4e030488051ebb6d78a5d3e3d
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-05-11 21:02:51 -0400

    added more button

--- pfp_maker.py
+++ pfp_maker.py
@@ -6,8 +6,8 @@ def rand_hex():
     return f"#{randint(0, 0xFFFFFF):06x}"
 
 
-def generate_code(colour):
-    code = f"{colour}_"
+def generate_code(hex_val):
+    code = f"{hex_val}_"
     for i in range(7):
         for j in range(7):
             if grid[i][j] != "#FFFFFF":
@@ -18,7 +18,8 @@ def generate_code(colour):
     print(code)
 
 
-def new_pfp_colour(colour):
+def new_pfp_colour(hex_val):
+    global colour
     for i in range(7):
         for j in range(7):
             if grid[i][j] != "#FFFFFF":
@@ -26,22 +27,23 @@ def new_pfp_colour(colour):
                     test_frame,
                     width=50,
                     height=50,
-                    bg=colour,
+                    bg=hex_val,
                     highlightthickness=0
                 ).grid(row=i, column=j)
 
-    master.config(bg=colour)
-    generate_code(colour)
+    master.config(bg=hex_val)
+    generate_code(hex_val)
+    colour = hex_val
 
 
-def new_patt(colour):
+def new_patt(hex_val):
     for i in range(7):
         for j in range(3):
-            temp = choice([colour, "#FFFFFF"])
+            temp = choice([hex_val, "#FFFFFF"])
             grid[i][j] = temp
             grid[i][6-j] = grid[i][j]
 
-        grid[i][3] = choice([colour, "#FFFFFF"])
+        grid[i][3] = choice([hex_val, "#FFFFFF"])
 
     for i in range(7):
         for j in range(7):
@@ -53,12 +55,9 @@ def new_patt(colour):
                 highlightthickness=0
             ).grid(row=i, column=j)
 
-    master.config(bg=colour)
-    generate_code(colour)
+    master.config(bg=hex_val)
+    generate_code(hex_val)
 
-
-
-code = ""
 
 master = Tk()
 master.minsize(0, 0)
@@ -67,7 +66,10 @@ colour = rand_hex()
 grid = [[-1 for i in range(7)] for i in range(7)]
 
 buttons_frame = Frame(master, height=40)
-buttons_frame.pack(fill=X)
+buttons_frame.grid(row=0, column=0, sticky=N+E+S+W)
+buttons_frame.grid_columnconfigure(0, minsize=1, weight=1)
+buttons_frame.grid_columnconfigure(1, minsize=1, weight=1)
+buttons_frame.grid_columnconfigure(2, minsize=1, weight=1)
 
 test_frame = Frame(
     master,
@@ -75,21 +77,27 @@ test_frame = Frame(
     width=420,
     bg="#000000"
 )
-test_frame.pack(fill=X, padx=35, pady=35)
+test_frame.grid(row=1, column=0, sticky=N+E+S+W)
 
 b1 = Button(
     buttons_frame,
     text="New Pattern",
     command=lambda: new_patt(colour)
 )
-b1.pack(side=LEFT, fill=X, expand=1)
+b1.grid(row=0, column=0, sticky=N+E+S+W)
 
 b2 = Button(
     buttons_frame,
     text="New Colour",
     command=lambda: new_pfp_colour(rand_hex())
 )
-b2.pack(side=RIGHT, fill=X, expand=1)
+b2.grid(row=0, column=1, sticky=N+E+S+W)
+
+b3 = Button(
+    buttons_frame,
+    text="More",
+)
+b3.grid(row=0, column=2, sticky=N+E+S+W)
 
 new_patt(colour)
 
