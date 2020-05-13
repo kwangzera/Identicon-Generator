de41e34f102a76ecb166b9081deb46d15a37f4cb
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-05-13 04:42:06 -0400

    Input box displayed, not working yet.

--- pfp_maker.py
+++ pfp_maker.py
@@ -7,10 +7,14 @@ def createNewWindow():
     top2 = Toplevel(master)
     top2.title("Import and Export")
 
-    Label(top2, text=code).pack()
+    Label(top2, text="Export ID: ").grid(row=0, column=0)
+    Label(top2, text=code, fg="blue").grid(row=0, column=1)
     
+    Label(top2, text="Import ID: ").grid(row=1, column=0)
+    Entry(top2, text=code, fg="blue").grid(row=1, column=1, sticky=E+W, padx=2, pady=2)
+
     top2.resizable(False, False)    
-    # top2.transient(master)
+    top2.transient(master)
     top2.grab_set()
     master.wait_window(top2)
 
@@ -21,15 +25,16 @@ def rand_hex():
 
 def generate_code(hex_val):
     global code
-    code = f"{hex_val}_"
+    code = f"{hex_val}"
+
     for i in range(7):
+        code += "_"
+
         for j in range(7):
             if grid[i][j] != "#FFFFFF":
                 code += "0"
             else:
                 code += "1"
-
-
 
 def new_pfp_colour(hex_val):
     global colour
