40f31830d8eaf6da92e49894e39e2a51b8481679
GeeTransit <geetransit@gmail.com>
2020-05-29 19:35:12 -0400

    Fix many things
    Add global declarations
    Fix icon file path
    Only show window when ready
    Add taskbar icon
    Add run.bat to safely run app

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+echo creating virtual environment
+python -m venv venv
+call venv\Scripts\activate.bat
+
+echo installing requirements
+python -m pip install -r requirements.txt
+
+echo starting app
+start pythonw -m pfp_maker
